5b3ed3ca59913685cdcf028a8feffa9a36004616
[Andrew Core] <[adcore89@gmail.com]>
2021-04-20 20:57:43 -0400

    prototypes added for inventory

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -18,19 +18,19 @@ test('creates a player object' , () => {
 
 test("gets player's stats as an object", () => {
     const player = new Player('Dave');
-
+  
     expect(player.getStats()).toHaveProperty('potions');
     expect(player.getStats()).toHaveProperty('health');
     expect(player.getStats()).toHaveProperty('strength');
     expect(player.getStats()).toHaveProperty('agility');
-});
+  });
 
-test('gets inventory from player or returns false' ,() => {
+  test('gets inventory from player or returns false', () => {
     const player = new Player('Dave');
-
+  
     expect(player.getInventory()).toEqual(expect.any(Array));
-
-    player.invetory =[];
-
+  
+    player.inventory = [];
+  
     expect(player.getInventory()).toEqual(false);
-});
+  });
